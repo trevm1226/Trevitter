9d14a77ea63691aa8ba4330a3636f3a85cdb28ef
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-04-23 14:11:41 -0500

    fdg

--- Jonathan/index.php
+++ Jonathan/index.php
@@ -3,7 +3,6 @@ for ($i= 1; $i<=100; $i++){
   if((($i % 3) ===0) && ($i % 5 == 0)) echo "FIZZBUZZ <br>";
   else if(($i % 3) ===0) echo "FIZZ <br> ";
   else if($i % 5 == 0) echo "BUZZ <br>";
-
   else echo $i . "<br>";
 }
 ?>
