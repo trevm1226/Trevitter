2c1617a8b2116d4fa7b598db17f3d271db145942
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-04-23 14:05:29 -0500

    gfh

--- Jonathan/index.php
+++ Jonathan/index.php
@@ -1,5 +1,5 @@
 <?php
-for ($i= 0; $i<100; $i++){
+for ($i= 1; $i<=100; $i++){
   if((($i % 3) ===0) && ($i % 5 == 0)) echo "FIZZBUZZ <br>";
   else if(($i % 3) ===0) echo "FIZZ <br> ";
   else if($i % 5 == 0) echo "BUZZ <br>";
