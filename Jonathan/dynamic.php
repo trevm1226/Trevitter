da6f9f4f5c04c815caf9a901c4e80c6c0d72b34b
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-04-23 14:34:21 -0500

    nh

--- Jonathan/dynamic.php
+++ Jonathan/dynamic.php
@@ -1,2 +1,8 @@
 <?php
+for ($i= 1; $i<=100; $i++){
+  if((($i % 3) ===0) && ($i % 5 == 0)) echo "FIZZBUZZ <br>";
+  else if(($i % 3) ===0) echo "FIZZ <br> ";
+  else if($i % 5 == 0) echo "BUZZ <br>";
+  else echo $i . "<br>";
+}
 ?>
--- Jonathan/index.php
+++ Jonathan/index.php
@@ -1,8 +1,23 @@
-<?php
-for ($i= 1; $i<=100; $i++){
-  if((($i % 3) ===0) && ($i % 5 == 0)) echo "FIZZBUZZ <br>";
-  else if(($i % 3) ===0) echo "FIZZ <br> ";
-  else if($i % 5 == 0) echo "BUZZ <br>";
-  else echo $i . "<br>";
-}
-?>
+<html>
+  <head>
+    <title> Test.html </title>
+  </head>
+  <body>
+    <?php
+    $arr = array("Apple", "Bananas", "Pears", "Peaches");
+    echo '<div>';
+    foreach($arr as $x){
+
+        echo '<div style="width:150px;    height:100px; border-bottom:50px;  background:blue; border-style: solid; border-color: white; color:white;">';
+
+echo '<div style = "text-align:center; v-align:center; ">';
+        echo $x . "<br>" ;
+        echo '</div>';
+        echo '</div>';
+
+
+    }
+    echo '</div>';
+    ?>
+  </body>
+</html>
