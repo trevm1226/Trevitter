add5f492700adc07c27e1a3e6eb704c1d81f3e26
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-05-03 13:18:52 -0500

    dfg

---
+++ Jonathan/register.php
@@ -0,0 +1,45 @@
+<html>
+<link rel="stylesheet" href="style.css" >
+<h1>Registration Form</h1>
+<form>
+
+  <div>
+  <label for="fName"> First Name: </label>
+  <input id ="fName">
+</div>
+
+<div>
+<label for="lName"> Last Name: </label>
+<input id="lName">
+</div>
+
+<div>
+<label for="uName"> Username: </label>
+<input id="uName">
+</div>
+
+<div>
+<label for="email"> E-mail: </label>
+<input type = "email">
+</div>
+
+<div>
+<label for="password"> Password: </label>
+<input type = "password">
+</div>
+
+<div>
+<label for="cPassword"> Confirm Password: </label>
+<input type="cPassword">
+</div>
+
+<div>
+  <button type="submit"> submit that button </button>
+</div>
+
+
+</form>
+
+<?php
+?>
+</html>
